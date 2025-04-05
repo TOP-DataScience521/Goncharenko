96f010f9d72d20da47444900a2204de9f9fd7f29
Gennadiy <renardes@gmail.com>
2025-04-05 22:33:11 +0500

    2025.03.09: оформление

--- 2025.03.09/1.py
+++ 2025.03.09/1.py
@@ -1,10 +1,12 @@
 name = input("Введите имя")
-famali= input("Введите фамилию")
-age= input("Введите возраст")
+famali = input("Введите фамилию")
+age = input("Введите возраст")
 
 print(name + " " + famali + ", " + str(age))
+
 
 # Введите имя Sasha
 # Введите фамилию Ivanov
 # Введите возраст99
- # Sasha  Ivanov, 99
+#  Sasha  Ivanov, 99
+
