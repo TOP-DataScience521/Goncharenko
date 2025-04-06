fb960aa4161384249811404bbc84608a73fa1331
Gennadiy <renardes@gmail.com>
2025-04-06 20:07:09 +0500

    2025.03.09: проверено

--- 2025.03.09/4.py
+++ 2025.03.09/4.py
@@ -1,8 +1,8 @@
 number = int(input("Введите трехзначное число"))
 
-num1 = number//100
-num2 = number//10%10
-num3 = number%10
+num1 = number // 100
+num2 = number // 10 % 10
+num3 = number % 10
 
 sum = num1 + num2 + num3
 multi = num1 * num2 * num3
@@ -12,4 +12,7 @@ print(sum, multi)
 
 # Введите трехзначное число 234
 # 9 24
+
+
+# ИТОГ: 4/4
 
